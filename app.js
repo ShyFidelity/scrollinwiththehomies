f63ddc7c4df6a31a7edc17042321c7521d4334eb
jkworks <info@jilliankayworks.com>
2022-02-03 20:12:57 -0700

    scroll magic

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 let listItems = [...document.querySelectorAll('li')]
+
+new ScrollMagic.Scene({
+    triggerElement: "#trigger1",
+    triggerHook: 0.9, // show, when scrolled 10% into view
+    duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+})
+.setClassToggle("#reveal1", "visible") // add class to reveal
+.addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
 var controller = new ScrollMagic.Controller();
 
 
@@ -21,3 +32,4 @@ listItems.forEach(item =>{
 observer.observe(item);
 
 })
+
